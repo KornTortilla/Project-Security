b2f520d9aba4d00f457a40220869610b9ed38eb8
Jhoshua Ampo <ampojhoshua@gmail.com>
2025-02-22 15:55:40 -0800

    update projectile despawn

--- Assets/Scripts/Weapon/MouthGun.cs
+++ Assets/Scripts/Weapon/MouthGun.cs
@@ -8,7 +8,7 @@ public class MouthGun : Weapon
     [SerializeField] private Transform muzzlePosition;
     [SerializeField] private float cooldownWindow = 0.1f;
 
-    private IObjectPool<SimpleProjectile> projectilePool;
+    private IObjectPool<Projectile> projectilePool;
 
     [SerializeField] private bool collectionCheck = true;
     [SerializeField] private int defaultCapacity = 20;
@@ -17,13 +17,13 @@ public class MouthGun : Weapon
     private float nextTimeToShoot;
 
     private void Awake() {
-        projectilePool = new ObjectPool<SimpleProjectile>(CreateProjectile, OnGetFromPool, OnReleaseToPool, OnDestroyPooledObject, collectionCheck, defaultCapacity, maxSize);
+        projectilePool = new ObjectPool<Projectile>(CreateProjectile, OnGetFromPool, OnReleaseToPool, OnDestroyPooledObject, collectionCheck, defaultCapacity, maxSize);
     }
 
     public bool Shoot()
     {
         if (Time.time > nextTimeToShoot && projectilePool != null) {
-            SimpleProjectile projectileObj = projectilePool.Get();
+            Projectile projectileObj = projectilePool.Get();
 
             if (projectileObj == null) return false;
 
@@ -46,15 +46,15 @@ public class MouthGun : Weapon
         return projectileInstance;
     }
 
-    private void OnGetFromPool(SimpleProjectile pooledObject) {
+    private void OnGetFromPool(Projectile pooledObject) {
         pooledObject.gameObject.SetActive(true);
     }
 
-    private void OnReleaseToPool(SimpleProjectile pooledObject) {
+    private void OnReleaseToPool(Projectile pooledObject) {
         pooledObject.gameObject.SetActive(false);
     }
 
-    private void OnDestroyPooledObject(SimpleProjectile pooledObject) {
+    private void OnDestroyPooledObject(Projectile pooledObject) {
         Destroy(pooledObject.gameObject);
     }    
 }
--- Assets/Scripts/Weapon/SimpleProjectile.cs
+++ Assets/Scripts/Weapon/SimpleProjectile.cs
@@ -2,31 +2,7 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.Pool;
 
-public class SimpleProjectile : MonoBehaviour
+public class SimpleProjectile : Projectile
 {
-    [SerializeField] private float timeoutDelay = 3f;
-    public IObjectPool<SimpleProjectile> ObjectPool { set => objectPool = value; }
-    public float DamageVal = 0f;
-    private IObjectPool<SimpleProjectile> objectPool;
-
-    public void Deactivate() {
-        StartCoroutine(DeactivateRoutine(timeoutDelay));
-    }
-
-    IEnumerator DeactivateRoutine(float delay) {
-        yield return new WaitForSeconds(delay);
-
-        Rigidbody rBody = GetComponent<Rigidbody>();
-        rBody.linearVelocity = new Vector3(0f, 0f, 0f);
-        rBody.angularVelocity = new Vector3(0f, 0f, 0f);
-
-        objectPool.Release(this);
-    }
-
-    void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Player")) {
-            other.GetComponent<EntityHealth>().TakeDamage(DamageVal);
-        }
-    }
+    
 }
