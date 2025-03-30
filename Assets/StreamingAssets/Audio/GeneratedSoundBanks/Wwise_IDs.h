3c5853c457c791395a1df695e59338d57f4fabe2
KornTortilla <ryanagundez@gmail.com>
2025-03-29 17:56:29 -0700

    Wwise Integration and Music Phase Test
    AAAAAAAAAAAAAAAAAAAAAAAAA

---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,56 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_BEACH = 2006186431U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace COMBAT_PHASES
+        {
+            static const AkUniqueID GROUP = 3280474060U;
+
+            namespace STATE
+            {
+                static const AkUniqueID IN_COMBAT = 2116791127U;
+                static const AkUniqueID LEAD_UP = 2543601261U;
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID OUT_OF_COMBAT = 3162566204U;
+                static const AkUniqueID PERFECT = 2161557176U;
+            } // namespace STATE
+        } // namespace COMBAT_PHASES
+
+    } // namespace STATES
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MUSIC = 3991942870U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
