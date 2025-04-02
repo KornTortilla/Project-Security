9f2c6da156af4b4258edc83c7de554b60d5c9e86
Jhoshua Ampo <ampojhoshua@gmail.com>
2025-04-01 22:57:53 -0700

    implement doors

---
+++ Assets/Scripts/Rooms/DoorBehavior.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+public class DoorBehavior : MonoBehaviour
+{
+    MeshRenderer meshRenderer;
+    [SerializeField] private float delay = 5f;
+    bool processingClose = true;
+
+    private void Awake() {
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (!other.CompareTag("Player")) return;
+        OpenDoor();
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (!other.CompareTag("Player")) return;
+        if (processingClose) {
+            StopCoroutine(CloseDoor());
+            processingClose = false;
+            meshRenderer.enabled = false;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (!other.CompareTag("Player")) return;
+        StartCoroutine(CloseDoor());
+    }
+
+    private void OpenDoor()
+    {
+        meshRenderer.enabled = false;
+        Vector3 newPos = transform.position;
+        newPos.y -= 4;
+        transform.position = newPos;
+    }
+
+    private IEnumerator CloseDoor() {
+        yield return new WaitForSeconds(delay);
+        if (!processingClose) yield return null;
+
+        Vector3 newPos = transform.position;
+        newPos.y += 4;
+        transform.position = newPos;
+        meshRenderer.enabled = true;
+    }
+}
